b37c739c2af3c419b43448d5a6f556378f7029bc
helloworld <helloworld@gmail.com>
2024-05-13 20:28:38 -0400

    first commit

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -5,7 +5,8 @@ function genDate(){
     let dat = new Date()
     return `${dat.getHours()}:${dat.getMinutes()}, ${dat.getDate()}/${dat.getMonth()}/${dat.getFullYear()}`
 }
-
+// user qismi===========================================
+// USER REGISTR
 export const register = async (req, res) =>{
     try {
         const {username, email, password} = await req.body[0]
@@ -26,15 +27,77 @@ export const register = async (req, res) =>{
     }
 }
 
+// USER LOGIN
+export const login = async (req, res) =>{
+    try{
+        const {email, password} = req.body[0]
+        let data = await query(`SELECT * FROM users WHERE email = $1 AND password = $2`, [email, password])
+        if(data.rows.length != 0){
+            res.status(200).send("SUCCESS loged in")
+        }
+        else{
+            res.send("No such user")
+        }
+    }catch(err){
+        res.send("An error reading data")
+    }
+}
+// =====================================================
 
 
 
+// BOOKS================================================
+export const addBook = async (req, res) => {
+    try {
+        let {title, author, genre, username} = req.body[0]
+        let tcheck = await query(`SELECT * FROM books WHERE title = $1`, [title])
+        if(tcheck.length != 0){
+            res.send("This book already exists")
+        }else{
+            if(title && author && genre && username){
+                username = await query(`SELECT id FROM users WHERE name = $1`, [username])
+                await query(`INSERT INTO books(title, author, publication_date, genre, user_id) VALUES($1, $2, $3, $4, $5)`, [title, author, genDate(), genre, username.rows[0].id])
+                res.send("added")
+            }
+        }
+    } catch (err) {
+        res.send(`${err}`)
+    }
+}
 
+// GET ALL BOOKS
+export const allBookData = async (req, res) => {
+    try {
+        let result = await query(`SELECT * FROM books`)
+        res.send(result.rows)
+    } catch (err) {
+        res.send("Couldn't read data from database")
+    }
+}
 
+// GET BOOK BY ID
+export const getBookById = async (req, res) => {
+    try {
+        let id = req.params.id
+        let result = await query(`SELECT * FROM books WHERE id = $1`, [id])
+        if(result.rows.length == 0){
+            res.send(`No book with id ${id}`)
+        }
+        else
+            res.send(result.rows)
+    } catch (err) {
+        res.send("Error while getting data from DB")
+    }
+}
 
-
-
-
+// PUT BOOK DATA
+export const updateBook = async (req, res) => {
+    try {
+        
+    } catch (err) {
+        res.send("An error while updating data")
+    }
+}
 // try{
 //     query("CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(30), email VARCHAR(40),password VARCHAR(30), created_at VARCHAR(40))")
 //     query("CREATE TABLE books(id SERIAL PRIMARY KEY, title VARCHAR(30), author VARCHAR(40), publication_date VARCHAR(40), genre VARCHAR(30), user_id INT, FOREIGN KEY(user_id) REFERENCES users(id))")
--- routes/users.router.js
+++ routes/users.router.js
@@ -1,8 +1,19 @@
 import { Router } from "express";
-import { register } from "../controllers/users.controller.js"
+import { register, login, addBook, allBookData, getBookById, updateBook } from "../controllers/users.controller.js"
 
 const router = Router();
 
+// USERS
 router.post("/users/register", register)
+router.post("/users/login", login)
+// ========================================
 
+
+// BOOKS
+router.post("/books", addBook)
+router.get("/books", allBookData)
+router.get("/books/:id", getBookById)
+router.put("/books/:id", updateBook)
+
+// ========================================
 export default router;
